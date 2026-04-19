5b1c52f36cb3f36fe71d928924e6f2b259e06bab
Lucas Gabriel da Silva Souza <lcgabriell82@gmail.com>
2026-04-19 20:42:03 -0300

    first commit

---
+++ src/test/java/com/app/Cria/CriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.Cria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
